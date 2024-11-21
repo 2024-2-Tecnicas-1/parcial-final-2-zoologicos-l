929e8dccd4db52f4abddf3810cc22a0efae2b986
sebastian <sabc1087@gmail.com>
2024-11-21 18:41:34 -0500

    nuevo1

--- src/main/java/logicaNegocio/Animal.java
+++ src/main/java/logicaNegocio/Animal.java
@@ -1,5 +1,30 @@
 package logicaNegocio;
 
 public abstract class Animal {
-    // TODO: Aquí va tu código
+    
+    private String emitirSonido;
+    private String obtenerDieta;
+
+    public Animal(String emitirSonido) {
+        this.emitirSonido = emitirSonido;
+        this.obtenerDieta = obtenerDieta;
+    }
+
+    public String getEmitirSonido() {
+        return emitirSonido;
+    }
+
+    public void setEmitirSonido(String emitirSonido) {
+        this.emitirSonido = emitirSonido;
+    }
+
+    public String getObtenerDieta() {
+        return obtenerDieta;
+    }
+
+    public void setObtenerDieta(String obtenerDieta) {
+        this.obtenerDieta = obtenerDieta;
+    }
+    
 }
+
